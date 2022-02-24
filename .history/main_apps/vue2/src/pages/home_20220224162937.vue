6aac274dafce315deb4d2dbe7060c234e9fb16e2
panqiang1 <panqiang1@midea.com>
2022-02-24 19:40:14 +0800

    路由跳转问题重现

---
+++ .history/main_apps/vue2/src/pages/home_20220224162937.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>主应用 - Vue@{{version}}</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button @click="goMicroAppPage"></button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+import Vue from 'vue'
+
+export default {
+  name: 'page1',
+  data () {
+    return {
+      version: Vue.version,
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    goMicroAppPage() {
+      this.$router.push('/app-vue2')
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
